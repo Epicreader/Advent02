0f1fce45075325f8a04b53b67bd3d60a35dbc8f6
Trent <epicreader0467@gmail.com>
2020-03-10 15:29:52 -0700

    Started
    I started part 2 it took me a while to understand what it was asking me to do but after I did I was able to start I don't know why it is not working know but I will figure it out.

--- script.js
+++ script.js
@@ -22,5 +22,44 @@ for (i = 0; i < opcode.length; i += 4) {
         break;
     }
 
+
 }
-console.log(opcode);
+//part 2
+var noun = 0;
+var verb = 0;
+var opcodes = [1, noun, verb, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 6, 19, 23, 1, 13, 23, 27, 1, 6, 27, 31, 1, 31, 10, 35, 1, 35, 6, 39, 1, 39, 13, 43, 2, 10, 43, 47, 1, 47, 6, 51, 2, 6, 51, 55, 1, 5, 55, 59, 2, 13, 59, 63, 2, 63, 9, 67, 1, 5, 67, 71, 2, 13, 71, 75, 1, 75, 5, 79, 1, 10, 79, 83, 2, 6, 83, 87, 2, 13, 87, 91, 1, 9, 91, 95, 1, 9, 95, 99, 2, 99, 9, 103, 1, 5, 103, 107, 2, 9, 107, 111, 1, 5, 111, 115, 1, 115, 2, 119, 1, 9, 119, 0, 99, 2, 0, 14, 0];
+
+var recentAnswer = 0;
+while (noun < 100) {
+    for (i = 0; i < opcodes.length; i += 4) {
+        // This is so that I have the original opcode[i] saved to be used and influenced more than once 
+        var zero = opcodes[i]
+        //This takes the value of the place of the second number in the set of four
+        var one = opcodes[opcodes[i + 1]];
+        //This takes the value of the place of the third number in the set of four
+        var two = opcodes[opcodes[i + 2]];
+        // This gets the place where the new number is going to go
+        var three = opcodes[i + 3];
+        // This if statement checks if the original opcode[i] of each opcode set is equal to 1, 2, or 99
+        if (zero === 1) {
+            //If it was equal to one it adds
+            opcodes[three] = one + two;
+        } else if (zero === 2) {
+            //if it was equal to 2 it multiplies
+            opcodes[three] = one * two;
+            //if it was equal to 99 it ends
+        }
+    }
+    if (noun == 99) {
+        noun = 0;
+        ++verb;
+    }
+    if (verb == 99) {
+        break;
+    }
+    if (opcodes[0] = 19690720) {
+        console.log(100 * noun + verb)
+        break;
+    }
+    ++noun;
+}
